04430bfd490a9af50f0933d9e66d9a63f62bf588
Yacoubi <yacoubi0abdelhakime@gmail.com>
2021-05-04 11:30:13 +0000

    v2:frontEnd-Ok-2

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="vue-tempalte">
+        <nav class="navbar shadow bg-white rounded justify-content-between flex-nowrap flex-row fixed-top">
+            <div class="container">
+                <img height="35" src="icons/logo.png" title="Storeino" alt="storeino">
+                <ul class="nav navbar-nav flex-row float-right">
+                <li class="nav-item">
+                    <router-link class="nav-link pr-3" to="/login">Sign in</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="btn btn-outline-primary" to="/">Sign up</router-link>
+                </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        }
+    }
+</script>
--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,19 +1,6 @@
 <template>
   <div >
-    <!-- Navigation <div class="vue-tempalte">-->
-    <nav class="navbar shadow bg-white rounded justify-content-between flex-nowrap flex-row fixed-top">
-      <div class="container">
-        <img height="35" src="icons/logo.png" title="Storeino" alt="storeino">
-        <ul class="nav navbar-nav flex-row float-right">
-          <li class="nav-item">
-            <router-link class="nav-link pr-3" to="/login">Sign in</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="btn btn-outline-primary" to="/">Sign up</router-link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <Header />
 
     <!-- Main -->
     <div class="App">
@@ -27,24 +14,17 @@
 </template>
  
 <script>
-
+import Header from "../components/Header"
 
 export default {
   name: 'App',
+  components:{
+    Header
+  }
 }
 
 </script>
 
 <style>
 
-/*
-#nav{
-	background-color: #777;
-	color: white;
-	height: 50px;
-	display: flex;
-	justify-content: flex-end;
-	align-items: center;
-
-} */
 </style>
